bab76103cb637faa2582f119f01c228284e3975b
Min Jae (Chris) Lee <minjaelee0727@gmail.com>
2021-10-02 22:35:16 -0400

    Initial Commit

---
+++ Shared/SensboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SensboxApp.swift
+//  Shared
+//
+//  Created by Min Jae Lee on 2021-10-02.
+//
+
+import SwiftUI
+
+@main
+struct SensboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
